8676c95cfc040399d363921f630cb77706b66064
Richard Di Cosmo <rjdicosmo@gmail.com>
2019-07-03 14:45:05 -0400

    updating branch

---
+++ src/main/java/com/revature/carappbeans/User.java
@@ -0,0 +1,98 @@
+package com.revature.carappbeans;
+
+import java.util.ArrayList;
+
+public class User {
+	
+	public static ArrayList<User> userList = new ArrayList<>();
+	
+	public User() {}
+	
+	public User(String userName, String passwrd, String name) {
+		super();
+		this.userName = userName;
+		this.passwrd = passwrd;
+		this.name = name;
+		this.access = false;
+	}
+
+	private String userName;
+	private String passwrd;
+	private String name;
+	private boolean access;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPasswrd() {
+		return passwrd;
+	}
+
+	public void setPasswrd(String passwrd) {
+		this.passwrd = passwrd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userName=" + userName + ", passwrd=" + passwrd + ", name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((passwrd == null) ? 0 : passwrd.hashCode());
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (passwrd == null) {
+			if (other.passwrd != null)
+				return false;
+		} else if (!passwrd.equals(other.passwrd))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+
+	public boolean isAccess() {
+		return access;
+	}
+
+	public void setAccess(boolean access) {
+		this.access = access;
+	}
+
+}
---
+++ src/main/java/com/revature/util/Customers.java
@@ -0,0 +1,29 @@
+package com.revature.util;
+
+import com.revature.carappbeans.Car;
+import com.revature.carappbeans.User;
+
+public class Customers implements Users {
+
+	public static double makeOffer(double offer) {
+		double currentOffer = offer;
+		return currentOffer;
+	}
+	
+	public static Car[] viewMyCars(User u) {
+		return null;
+	}
+	
+	public static double[] viewMyPayments(User u) {
+		return null;
+	}
+	
+	public User login(String username, String passwrd) {
+		
+		return null;
+	}
+	
+	public void logout() {
+		
+	}
+}
